86faa2da75c39ff600e53a1d788f14bd6ffb32c1
bbc6bae9 <chinahuanghong@gmail.com>
2023-01-25 23:47:12 +0800

    add watch app

---
+++ WatchLandMarks Watch App/WatchLandMarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  WatchLandMarksApp.swift
+//  WatchLandMarks Watch App
+//
+//  Created by huang on 2023/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
