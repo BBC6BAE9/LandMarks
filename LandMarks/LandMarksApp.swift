0079cff42a50d98a0c328a2d05e1310f02c84311
bbc6bae9 <chinahuanghong@gmail.com>
2023-01-25 14:22:31 +0800

    initial commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by huang on 2023/1/25.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
